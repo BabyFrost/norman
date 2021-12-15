02a9f054d274cb2789067255e55c54c013d73163
Voufack Harold <voufackharold@gmail.com>
2021-12-15 17:07:53 +0100

    Coded Initial Authentication interfaces - Login - Registration

---
+++ norman/norman/Connexion_Inscription2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace norman
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Connexion_Inscription2 : ContentPage
+    {
+        public Connexion_Inscription2()
+        {
+            InitializeComponent();
+        }
+    }
+}
